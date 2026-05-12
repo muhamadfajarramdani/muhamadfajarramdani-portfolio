f28b83cbe2e424ae566aee8e3ef3e1776e3f75b1
muhamadfajarramdani <muhamadfajarramdani@smkwikrama.sch.id>
2026-05-12 14:47:44 +0700

    fix package

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID as string)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
